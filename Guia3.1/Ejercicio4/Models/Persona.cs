ba1f2d4fba8f9af710206990c7988d1af7b1381a
marti-espeleta-rojas <martiespeletarojas@gmail.com>
2024-08-28 13:05:46 -0300

    Subida Ejercicio 4 de Guia 3

---
+++ Guia3.1/Ejercicio4/Models/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    abstract public class Persona
+    {
+        public string ApellidosNombre { get; set; }
+
+        public Persona(string nombre)
+        {
+            ApellidosNombre = nombre;
+        }
+    }
+}
